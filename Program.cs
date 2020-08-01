9d54f46ff05b8987195cc140f2b6ac1536370d76
DorkyTay <67008340+DorkyTay@users.noreply.github.com>
2020-07-31 21:10:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LAB_9_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from LAB_9_4");
+
+            HtmlPageBuilder htmlPageBuilder = new HtmlPageBuilder();
+            htmlPageBuilder.CollectThenCreateHTML();
+
+        }
+    }
+}
